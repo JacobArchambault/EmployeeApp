5ff34fbb7f749e5102bf69398062fc5aeebb192f
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 17:05:48 -0400

    Split Employee class into two partial classes.

--- EmployeeApp/Employee.cs
+++ EmployeeApp/Employee.cs
@@ -6,26 +6,8 @@ using System.Threading.Tasks;
 
 namespace EmployeeApp
 {
-    class Employee
+    partial class Employee
     {
-        // Field data.
-        private string empName;
-        private int empID;
-        private float currPay;
-        private int empAge;
-
-        // Constructors.
-        public Employee() { }
-        public Employee(string name, int id, float pay)
-            : this(name, 0, id, pay) { }
-        public Employee(string name, int age, int id, float pay)
-        {
-            Name = name;
-            Age = age;
-            ID = id;
-            Pay = pay;
-        }
-
         // Methods.
         public void GiveBonus(float amount)
         {Pay += amount;}
