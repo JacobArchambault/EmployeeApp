3b038ee080a94debaf759ffe6ae6aaa57c2d463e
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 13:05:15 -0400

    Add Constructors, getters and setters for Employee.cs

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        // Accessor (get method)
+        public string GetName()
+        {
+            return empName;
+        }
+
+        // Mutator (set method)
+        public void SetName(string name)
+        {
+            // Do a check on incoming value before making assignment
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name length exceeds 15 characters!");
+            else
+                empName = name;
+        }
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -10,6 +10,20 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30_000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // Use the get/set methods to interact with the object's name.
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+
+            // Longer than 15 characters! Error will print to console.
+            Employee emp2 = new Employee();
+            emp2.SetName("Xena the warrior princess");
+
+            Console.ReadLine();
         }
     }
 }
