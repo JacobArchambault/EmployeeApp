c35a6f42b1683e1ece241065f44de32bd5ec56e0
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 13:25:18 -0400

    Add Age field to Employee; update its constructor and DisplayStats() method.

--- EmployeeApp/Employee.cs
+++ EmployeeApp/Employee.cs
@@ -12,31 +12,51 @@ namespace EmployeeApp
         private string empName;
         private int empID;
         private float currPay;
+        private int empAge;
 
         // Constructors.
         public Employee() { }
         public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay) { }
+        public Employee(string name, int age, int id, float pay)
         {
             empName = name;
             empID = id;
+            empAge = age;
             currPay = pay;
         }
 
-        // Accessor (get method)
-        public string GetName()
+        // Properties
+        public string Name
         {
-            return empName;
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
         }
 
-        // Mutator (set method)
-        public void SetName(string name)
+        public int ID
         {
-            // Do a check on incoming value before making assignment
-            if (name.Length > 15)
-                Console.WriteLine("Error! Name length exceeds 15 characters!");
-            else
-                empName = name;
+            get { return empID; }
+            set { empID = value; }
         }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
 
         // Methods.
         public void GiveBonus(float amount)
@@ -48,6 +68,7 @@ namespace EmployeeApp
         {
             Console.WriteLine("Name: {0}", empName);
             Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
             Console.WriteLine("Pay: {0}", currPay);
         }
     }
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -12,16 +12,15 @@ namespace EmployeeApp
         {
             Console.WriteLine("***** Fun with Encapsulation *****\n");
             Employee emp = new Employee("Marvin", 456, 30_000);
+            Employee joe = new Employee();
+            joe.Age++;
+
             emp.GiveBonus(1000);
             emp.DisplayStats();
 
-            // Use the get/set methods to interact with the object's name.
-            emp.SetName("Marv");
-            Console.WriteLine("Employee is named: {0}", emp.GetName());
-
-            // Longer than 15 characters! Error will print to console.
-            Employee emp2 = new Employee();
-            emp2.SetName("Xena the warrior princess");
+            // Reset and then get the Name property.
+            emp.Name = "Marv";
+            Console.WriteLine("Employee is named: {0}", emp.Name);
 
             Console.ReadLine();
         }
